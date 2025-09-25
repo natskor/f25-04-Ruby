92f86202f90761bd2ccbaab2749a772e0a2a8912
Colin McGowan <cmcgo005@odu.edu>
2025-09-25 15:11:13 -0400

    font brokey, fix(?) & GUI tomorrow

--- src/rewards_store.py
+++ src/rewards_store.py
@@ -2,3 +2,16 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Rewards Store"
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+    
+    page.fonts = {
+    "LibreBaskerville": "assets/fonts/LibreBaskerville-Regular.ttf",
+    "LibreBaskerville-Bold": "assets/fonts/LibreBaskerville-Bold.ttf"
+    }
+
+    #currently not displaying font accurately, will look @ later
+    #page.theme = ft.Theme(font_family="LibreBaskerville")
+    #page.add(ft.Text("Test Text #1", size=40"))
+
+ft.app(main, assets_dir="assets/fonts")
