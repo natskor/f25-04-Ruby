01efcb80a422b1638fadd802f03cc0b7ff5a174b
Colin McGowan <cmcgo005@odu.edu>
2025-10-30 21:45:52 -0400

    need to push

--- src/backend/firestore.py
+++ src/backend/firestore.py
@@ -1,5 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
+### Access to Firestore
 #cred = credentials.Certificate("path/to/serviceAccountKey.json")
 #firebase_admin.initialize_app(cred)
+
+### Database Object
+#db = firestore.client()
+
