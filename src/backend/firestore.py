eff0578c9ae3b75a99285c1978bdea61103491c8
Colin McGowan <cmcgo005@odu.edu>
2025-10-31 14:47:49 -0400

    Firestore connection

--- src/backend/firestore.py
+++ src/backend/firestore.py
@@ -1,11 +1,33 @@
+import os
+from dotenv import load_dotenv
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+### Load our environment variable
+load_dotenv()
+### Get the environment variable
+service_account = os.getenv("GOOGLE_FS_DB_CREDS")
+
+### I used a relative path, so this should only apply if you do as well
+if not os.path.isabs(service_account):
+    service_account = os.path.abspath(service_account)
+
 ### Access to Firestore
-#cred = credentials.Certificate("path/to/serviceAccountKey.json")
-#firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(service_account)
+firebase_admin.initialize_app(cred)
 
 ### Database Object
-#db = firestore.client()
+db = firestore.client()
 
+###########
+# Example #
+###########
+
+# 1) Creating data
+#doc_ref = db.collection("users").document("alovelace")
+#doc_ref.set({"first": "Ada", "last": "Lovelace", "Born": 1815})
+
+# 2) Clean up the data
+db.collection("users").document("alovelace").delete()
