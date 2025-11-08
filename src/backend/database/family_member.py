440ab34226117fe7e09062c45d4e541e31323fb5
Colin McGowan <cmcgo005@odu.edu>
2025-11-08 18:19:27 -0500

    file structure

---
+++ src/backend/database/family_member.py
@@ -0,0 +1,40 @@
+from firestore import db
+
+### ADD DATA
+
+# Member ID (supplied email)
+
+
+# First Name
+
+
+# Last Name
+
+
+# Role
+
+
+# PIN
+
+
+# Family ID (N/A right now)
+
+### RETRIEVE DATA
+
+# Member ID (supplied email)
+
+
+# First Name
+
+
+# Last Name
+
+
+# Role
+
+
+# PIN
+
+
+# Family ID (N/A right now)
+
