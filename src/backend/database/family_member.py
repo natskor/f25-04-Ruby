5d86b74d966e4d7456d2c6018b69ef9859e5b8e5
Colin McGowan <cmcgo005@odu.edu>
2025-11-08 19:28:05 -0500

    slight changes

--- src/backend/database/family_member.py
+++ src/backend/database/family_member.py
@@ -1,23 +1,34 @@
-from firestore import db
+from firestore import db as DB
+
+##### DOCUMENT INITIALIZATION
+doc_ref = DB.collection("FAMILY_MEMBER").document("KidOne")
+doc_ref.set({ "MemberID": 1,
+              "First": "Kid",
+              "Last": "One",
+              "Role": "child",
+              "PIN": 12345,
+              "FamilyID": 1 })
 
 ### ADD DATA
 
 # Member ID (supplied email)
-
+#def add_member_id():
 
 # First Name
-
+#def add_first_name():
 
 # Last Name
-
+#def add_last_name():
 
 # Role
-
+#def add_role():
 
 # PIN
-
+#def add_pin():
 
 # Family ID (N/A right now)
+#def add_family_id():
+
 
 ### RETRIEVE DATA
 
