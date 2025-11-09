17e3e088c73937d545d2bab3317021647c96ef98
Colin McGowan <cmcgo005@odu.edu>
2025-11-08 20:40:45 -0500

    `add` members - family_member.py

--- src/backend/database/family_member.py
+++ src/backend/database/family_member.py
@@ -1,51 +1,62 @@
 from firestore import db as DB
 
 ##### DOCUMENT INITIALIZATION
-doc_ref = DB.collection("FAMILY_MEMBER").document("KidOne")
-doc_ref.set({ "MemberID": 1,
-              "First": "Kid",
-              "Last": "One",
-              "Role": "child",
-              "PIN": 12345,
-              "FamilyID": 1 })
-
-### ADD DATA
+def create_member(email: str):
+    new_member = DB.collection("FAMILY MEMBER").document(email)
+    new_member.set({ 
+                     "MemberID": email,
+                     "First":    "",
+                     "Last":     "",
+                     "Role":     "",
+                     "PIN":      0000,
+                     "FamilyID": 0 
+                    })
 
 # Member ID (supplied email)
-#def add_member_id():
+def add_member_id(email: str):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"MemberID": email})
 
 # First Name
-#def add_first_name():
+def add_first_name(email: str, first: str):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"First": first})
 
 # Last Name
-#def add_last_name():
+def add_last_name(email: str, last: str):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"Last": last})
 
 # Role
-#def add_role():
+def add_role(email: str, role: str):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"Role": role})
 
 # PIN
-#def add_pin():
+def add_pin(email: str, pin: int):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"PIN": pin})
 
 # Family ID (N/A right now)
-#def add_family_id():
-
+def add_family_id(email: str, family_id: int):
+    member = DB.collection("FAMILY MEMBER").document(email)
+    member.update({"FamilyID": family_id})
 
 ### RETRIEVE DATA
-
 # Member ID (supplied email)
-
+#def get_member_id(email: str):
 
 # First Name
-
+#def get_first_name(email: str):
 
 # Last Name
-
+#def get_last_name(email: str):
 
 # Role
-
+#def get_role(email: str):
 
 # PIN
-
+#def get_pin(email: str):
 
 # Family ID (N/A right now)
-
+#def get_family_id(email: str):
