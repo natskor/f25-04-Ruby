662e37aadb07b8641a25a87be12cc2a204306ce0
Caitlyn Heath <cheat006@odu.edu>
2025-11-12 14:44:13 -0500

    fixed format for main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,19 +1,8 @@
 # FastAPI app
 
 from fastapi import FastAPI
-from backend.routes import collabrewards_routes, signup_routes, login_routes, calendar_routes, rewards_store_routes, chore_routes, progress_routes, pin_avatar_routes
-# from routes import (
-#     collabrewards_routes,
-#     signup_routes,
-#     login_routes,
-#     calendar_routes,
-#     rewards_store_routes,
-#     chore_routes,
-#     auth_routes,
-#     progress_routes,
-#     pin_avatar_routes,
-# )
-
+from backend.routes import collabrewards_routes, signup_routes, login_routes, calendar_routes
+from backend.routes import rewards_store_routes, chore_routes, progress_routes, pin_avatar_routes
 
 app = FastAPI()
 
@@ -23,9 +12,12 @@ app.include_router(login_routes.router)
 app.include_router(calendar_routes.router)
 app.include_router(rewards_store_routes.router)
 app.include_router(chore_routes.router)
-# app.include_router(auth_routes.router)
 app.include_router(progress_routes.router)
 app.include_router(pin_avatar_routes.router)
+
+# Ignore for now
+# app.include_router(auth_routes.router)
+
 # @app.get("/health")
 # def health():
 #     return {"ok": True}
