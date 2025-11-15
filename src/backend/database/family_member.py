2fce0cd301dc2d2e3fbb11309ca6dd0991f13900
Colin McGowan <cmcgo005@odu.edu>
2025-11-14 21:16:42 -0500

    family_unit.py creation

--- src/backend/database/family_member.py
+++ src/backend/database/family_member.py
@@ -1,60 +1,90 @@
-from firestore import db as DB
-from google.cloud.firestore_v1.base_query import FieldFilter
+################################
+
+# File will not likely be needed
+# Keeping just in case
+
+################################
+
+#from firestore import db as DB
+#from google.cloud.firestore_v1.base_query import FieldFilter
+
+################################
 
 ### DOCUMENT INITIALIZATION
-def create_member(email: str):
-    new_member = DB.collection("FAMILY MEMBER").document(email)
-    new_member.set({ 
-                     "MemberID": email,
-                     "First":    "",
-                     "Last":     "",
-                     "Role":     "",
-                     "PIN":      0000,
-                     "FamilyID": 0 
-                    })
+
+################################
+
+#def create_member(email: str):
+#    new_member = DB.collection("FAMILY MEMBER").document(email)
+#    new_member.set({ 
+#                     "MemberID": email,
+#                     "First":    "",
+#                     "Last":     "",
+#                     "Role":     "",
+#                     "PIN":      0000,
+#                     "FamilyID": 0 
+#                    })
+
+################################
 
 ### ADD DATA
 
+################################
+
 # Member ID (supplied email)
-def add_member_id(email: str):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"MemberID": email})
+#def add_member_id(email: str):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"MemberID": email})
+
+################################
 
 # First Name
-def add_first_name(email: str, first: str):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"First": first})
+#def add_first_name(email: str, first: str):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"First": first})
+
+################################
 
 # Last Name
-def add_last_name(email: str, last: str):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"Last": last})
+#def add_last_name(email: str, last: str):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"Last": last})
+
+################################
 
 # Role
-def add_role(email: str, role: str):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"Role": role})
+#def add_role(email: str, role: str):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"Role": role})
+
+################################
 
 # PIN
-def add_pin(email: str, pin: int):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"PIN": pin})
+#def add_pin(email: str, pin: int):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"PIN": pin})
+
+################################
 
 # Family ID (N/A right now)
-def add_family_id(email: str, family_id: int):
-    member = DB.collection("FAMILY MEMBER").document(email)
-    member.update({"FamilyID": family_id})
+#def add_family_id(email: str, family_id: int):
+#    member = DB.collection("FAMILY MEMBER").document(email)
+#    member.update({"FamilyID": family_id})
+
+################################
 
 ### RETRIEVE DATA
 
 # Provide: the member's email.
 # Returns: formatted information of member information.
-def get_member_info(email: str):
-    search = DB.collection("FAMILY MEMBER")
-    result = search.where(filter=FieldFilter("MemberID", "==", email)).stream()
+#def get_member_info(email: str):
+#    search = DB.collection("FAMILY MEMBER")
+#    result = search.where(filter=FieldFilter("MemberID", "==", email)).stream()
     
-    for doc in result:
-        print(f"{doc.id} => {doc.to_dict()}")
+#    for doc in result:
+#        print(f"{doc.id} => {doc.to_dict()}")
+
+################################
 
 # LOGGING OFF, WORK IN PROGRESS!
 # Provide: the member's email & desired "column".
@@ -65,3 +95,5 @@ def get_member_info(email: str):
 #    
 #    for doc in result:
 #        print(f"{doc.to_dict()}")
+
+################################
