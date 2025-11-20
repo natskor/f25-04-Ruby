bfe4f5e126f880f2a2c53c499e1e7a0e429a8d30
Colin McGowan <cmcgo005@odu.edu>
2025-11-19 21:32:07 -0500

    rid .env.txt , family_unit SELECT functions

--- src/backend/database/family_unit.py
+++ src/backend/database/family_unit.py
@@ -1,4 +1,4 @@
-from backend.database.firestore import db as DB
+from firestore import db as DB
 from google.cloud.firestore_v1.base_query import FieldFilter
 
 ### Initialization.
@@ -12,12 +12,11 @@ def create_family(email: str):
                     })
 
 # New profile creation for family account.
-def create_profile(email: str, first: str):
+def create_profile(email: str, user: str):
     family_ref = DB.collection("FAMILY UNIT").document(email)
-    new_profile = family_ref.collection("PROFILE").document(first)
+    new_profile = family_ref.collection("PROFILE").document(user)
     new_profile.set({
-                     "First": first,
-                     "Last": "",
+                     "User": user,
                      "Role": "",
                      "PIN": 0000
                     })
@@ -30,48 +29,58 @@ def add_password(email: str, password: str):
 
 ### Add Data - PROFILE subcollection.
 
-def add_last(email: str, first: str, last: str):
-    profile_ref = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(first)
-    profile_ref.update({"Last": last})
-
-def add_role(email: str, first: str, role: str):
-    profile_ref = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(first)
+def add_role(email: str, user: str, role: str):
+    profile_ref = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(user)
     profile_ref.update({"Role": role})
-    
-def add_pin(email: str, first: str, pin: int):
-    profile_ref = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(first)
-    profile_ref.update({"Role": pin})
 
-### Get Data - FAMILY UNIT collection. -!!! Not working currently. Fix later.
+def add_pin(email: str, user: str, pin: int):
+    profile_ref = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(user)
+    profile_ref.update({"PIN": pin})
+
+### Get Data - FAMILY UNIT collection.
 
 def get_family_info(email: str):
-    search = DB.collection("FAMILY UNIT").document(email)
-    result = search.where(filter=FieldFilter("FamilyID", "==", email))
+    search = DB.collection("FAMILY UNIT")
+    result = search.where(filter=FieldFilter("FamilyID", "==", email)).stream()
     
     for doc in result:
         print(f"{doc.id} => {doc.to_dict()}")
 
-### Get Data - PROFILE subcollection. -!!! Not working currently. Fix later.
+def select_family_info(email: str, column: str):
+    search = DB.collection("FAMILY UNIT")
+    result = search.where(filter=FieldFilter("FamilyID", "==", email)).stream()
+    
+    for doc in result:
+        val = doc.to_dict()
+        print(f"{val[column]}")
 
-def get_profile_info(email: str, first: str):
-    search = DB.collection("FAMILY UNIT").document(email).collection("PROFILE").document(first)
-    result = search.where(filter=FieldFilter("First", "==", first))
+### Get Data - PROFILE subcollection.
+
+def get_profile_info(email: str, user: str):
+    search = DB.collection("FAMILY UNIT").document(email).collection("PROFILE")
+    result = search.where(filter=FieldFilter("User", "==", user)).stream()
     
     for doc in result:
         print(f"{doc.id} => {doc.to_dict()}")
     
+def select_profile_info(email: str, user: str, column: str):
+    search = DB.collection("FAMILY UNIT").document(email).collection("PROFILE")
+    result = search.where(filter=FieldFilter("User", "==", user)).stream()
+    
+    for doc in result:
+        val = doc.to_dict()
+        print(f"{val[column]}")
+
 ### Testing area...
 
-## FAMILY UNIT collection.
-#create_family("test@email")
-#add_password("test@email", "yolo")
+# Display ALL data from each, respectively.
+#get_family_info("TEST@email")
+#get_profile_info("TEST@email", "Chud")
 
-## PROFILE subcollection.
-#create_profile("test@email", "chud")
-#add_last("test@email", "chud", "life")
-#add_role("test@email", "chud", "child")
-#add_pin("test@email", "chud", 1234)
+# Display SELECTED data from each.
+#select_family_info("TEST@email", "FamilyID")
+#select_family_info("TEST@email", "Password")
 
-## Display data from each.
-#get_family_info("test@email")
-#get_profile_info("test@email", "chud")
+#select_profile_info("TEST@email", "Chud", "User")
+#select_profile_info("TEST@email", "Chud", "Role")
+#select_profile_info("TEST@email", "Chud", "PIN")
